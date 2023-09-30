52da93c1e625ab5135b770b2429870b4848363b8
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-09-30 16:27:23 +0700

    Fix minor

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,25 @@ import * as compression from 'compression';
 import { AppModule } from './app.module';
 import { ApiEntryGuard } from './guard/api-entry.guard';
 import { ApiKeyService } from './api-key/service/api-key.service';
+import { ConfigService } from '@nestjs/config';
 
 
 async function bootstrap() {
-
   const app = await NestFactory.create(
     AppModule,
   {
     logger: ['error', 'warn'],
   });
   
+  const configService = app.get(ConfigService);
+  const PORT:number = configService.get<number>('AUDIO_SERVER_PORT');
+
   app.use(helmet( {crossOriginResourcePolicy: false}));
   app.use(compression());
   app.useGlobalGuards(new ApiEntryGuard(new Reflector(),app.get(ApiKeyService)));
   app.setGlobalPrefix('api/audio-server');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000)
-  console.log(`Server running on port:4000`);
+  await app.listen(PORT);
+  console.log(`Server running on port : ${PORT}`);
 }
 bootstrap();
